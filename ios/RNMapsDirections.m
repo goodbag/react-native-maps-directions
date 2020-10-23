ef42b19c9084dbc12981af56697a693e039cd52c
Johannes Herrnegger <johannes.herrnegger@gmail.com>
2020-10-23 16:49:32 +0200

    fix(ios): bring integration to life

--- ios/RNMapsDirections.m
+++ ios/RNMapsDirections.m
@@ -6,37 +6,8 @@
 #import <React/RCTUtils.h>
 #import <React/RCTLog.h>
 
-@interface RCTConvert (Mapkit)
-
-+ (MKCoordinateSpan)MKCoordinateSpan:(id)json;
-+ (MKCoordinateRegion)MKCoordinateRegion:(id)json;
-
-@end
-
-@implementation RCTConvert(MapKit)
-
-+ (MKCoordinateSpan)MKCoordinateSpan:(id)json
+@implementation RNMapsDirections
 {
-    json = [self NSDictionary:json];
-    return (MKCoordinateSpan){
-        [self CLLocationDegrees:json[@"latitudeDelta"]],
-        [self CLLocationDegrees:json[@"longitudeDelta"]]
-    };
-}
-
-+ (MKCoordinateRegion)MKCoordinateRegion:(id)json
-{
-    return (MKCoordinateRegion){
-        [self CLLocationCoordinate2D:json],
-        [self MKCoordinateSpan:json]
-    };
-}
-
-@end
-
-@implementation RNDirections
-{
-    MKLocalSearch *localSearch;
     MKDirectionsRequest *directionRequest;
 }
 
@@ -47,38 +18,7 @@
 
 RCT_EXPORT_MODULE()
 
-- (NSArray *)formatLocalSearchCallback:(MKLocalSearchResponse *)localSearchResponse
-{
-    NSMutableArray *RCTResponse = [[NSMutableArray alloc] init];
-    
-    for (MKMapItem *mapItem in localSearchResponse.mapItems) {
-        NSMutableDictionary *formedLocation = [[NSMutableDictionary alloc] init];
-        
-        [formedLocation setValue:mapItem.name forKey:@"name"];
-        [formedLocation setValue:mapItem.url.absoluteURL forKey:@"absoluteUrl"];
-        [formedLocation setValue:mapItem.url.absoluteString forKey:@"absoluteStrUrl"];
-        if (@available(iOS 9.0, *)) {
-            [formedLocation setValue:mapItem.timeZone forKey:@"timeZone"];
-        } else {
-            // Fallback on earlier versions
-        }
-        [formedLocation setObject:[NSNumber numberWithBool:mapItem.isCurrentLocation] forKey:@"isCurrentLocation"];
-        if (@available(iOS 13.0, *)) {
-            [formedLocation setValue:mapItem.pointOfInterestCategory forKey:@"pointOfInterestCategory"];
-        } else {
-            // Fallback on earlier versions
-        }
-        [formedLocation setValue:mapItem.placemark.title forKey:@"address"];
-        [formedLocation setValue:@{@"latitude": @(mapItem.placemark.coordinate.latitude),
-                                   @"longitude": @(mapItem.placemark.coordinate.longitude)} forKey:@"location"];
-        
-        [RCTResponse addObject:formedLocation];
-    }
-    
-    return [RCTResponse copy];
-}
-
-RCT_EXPORT_METHOD(getRouteDetails:(NSDictionary *)placemarks callback:(RCTResponseSenderBlock)callback){
+RCT_EXPORT_METHOD(getRouteDetails:(NSDictionary *)placemarks mode:(NSString *)mode callback:(RCTResponseSenderBlock)callback){
     
 //    [directionRequest cancel];
     
@@ -95,10 +35,22 @@ RCT_EXPORT_METHOD(getRouteDetails:(NSDictionary *)placemarks callback:(RCTRespon
     MKMapItem *sourceMapItem = [[MKMapItem alloc] initWithPlacemark:placemarkOne];
     MKMapItem *destinationMapItem = [[MKMapItem alloc] initWithPlacemark:placemarkTwo];
     
+    MKDirectionsTransportType transportType;
+    if([mode isEqualToString:@"DRIVING"]) {
+        transportType = MKDirectionsTransportTypeAutomobile;
+    } else if([mode isEqualToString:@"WALKING"]) {
+        transportType = MKDirectionsTransportTypeWalking;
+    } else if([mode isEqualToString:@"TRANSIT"]) {
+        transportType = MKDirectionsTransportTypeTransit;
+    } else {
+        transportType = MKDirectionsTransportTypeAny;
+    }
+    // BICYCLING not available in Mapkit
+    
     directionRequest = [[MKDirectionsRequest alloc] init];
     [directionRequest setSource:sourceMapItem];
     [directionRequest setDestination:destinationMapItem];
-    [directionRequest setTransportType:MKDirectionsTransportTypeWalking];
+    [directionRequest setTransportType:transportType];
     [directionRequest setRequestsAlternateRoutes:NO];
     
     MKDirections *directions = [[MKDirections alloc] initWithRequest:directionRequest];
@@ -112,15 +64,8 @@ RCT_EXPORT_METHOD(getRouteDetails:(NSDictionary *)placemarks callback:(RCTRespon
             MKRoute *firstRoute = [[response routes] objectAtIndex:0];
                 NSArray *steps = firstRoute.steps;
                 for (MKRouteStep *step in steps) {
-                    NSLog(@"---------------------------------------------------------------------------------");
-                    NSLog(@"instruction %@", step.instructions);
-                    NSLog(@"notice %@", step.notice);
-                    NSLog(@"distance %f", step.distance);
-                    NSLog(@"latitude %f", step.polyline.coordinate.latitude);
-                    NSLog(@"longitude %f", step.polyline.coordinate.longitude);
-                    NSLog(@"---------------------------------------------------------------------------------");
                     NSDictionary *currentStep =@{
-                        @"instructions": step.instructions,
+                        @"instructions": (step.instructions) ? step.instructions : @"",
                         @"distance": [NSNumber numberWithDouble:step.distance],
                         @"latitude": [NSNumber numberWithDouble:step.polyline.coordinate.latitude],
                         @"longitude": [NSNumber numberWithDouble:step.polyline.coordinate.longitude],
@@ -134,7 +79,7 @@ RCT_EXPORT_METHOD(getRouteDetails:(NSDictionary *)placemarks callback:(RCTRespon
             NSDictionary *successDict =@{
                                          @"eta": [NSNumber numberWithDouble:route.expectedTravelTime],
                                          @"distance": [NSNumber numberWithDouble: route.distance],
-                                         @"waypoints": [RCTConvert NSArray:polylinePoints],
+                                         @"coordinates": [RCTConvert NSArray:polylinePoints],
                                          };
                                         
             
